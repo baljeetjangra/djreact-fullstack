a02979e98192fb76b381c7af207eaae6e73a6591
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-06-03 11:10:09 +0530

    project setup

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/baljeet/Projects/My Projects/Django+react/djreact/backend/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
